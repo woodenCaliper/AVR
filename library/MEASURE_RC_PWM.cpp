1c3a038a44062b0992f2a7bd39c7806c0519854d
woodenCaliper <cederfield@gmail.com>
2018-02-03 06:09:15 +0900

    delet MEASURE_RC_PWM and rename it

--- library/MEASURE_RC_PWM.cpp
+++ library/MEASURE_RC_PWM.cpp
@@ -1,3 +1,15 @@
+/*
+条件
+pwmのoffと次のpwmのONの間には7.28us以上必要
+  pwm1    pwm2        pwm1    pwm2
+__|---|___|--|________|---|___|--|__
+       <->                 <->
+       7.28us以上          7.28us以上
+計測ピンのほか、or回路を通して、インプットキャプチャPINに接続しなければならない
+PWM信号によって同時に複数ピンがONの瞬間があってはならない
+*/
+
+
 #ifndef MEASURE_PWM_CAPT_H_
 #define MEASURE_PWM_CAPT_H_
 
@@ -6,78 +18,93 @@
 #include <avr/interrupt.h>
 
 
+
 class MeasureRc{
 	public:
-	float bitToUsec;
-	uint16_t upTcnt, downTcnt;
-	uint16_t upCount, downCount;
-	uint32_t highBit, lowBit;
-	bool highUpdateFlag, lowUpdateFlag;
-	Pin pin;
+	struct timeDuration{
+		uint16_t startCount;
+		uint16_t startTcnt;
+		uint16_t endCount;
+		uint16_t endTcnt;
+	};
+	timeDuration onTime, bufferOnTime;
+	bool updateFlag;
+	avrPin measurePin;
 
-	static uint8_t pinNum;
+	static float bitToUsec;
+	static uint8_t measureNum;
 	static uint16_t nowCount;
 
-	MeasureRc(Pin pin);
-	void begin(uint16_t division);
+	MeasureRc(avrPin pin);
+	static void begin(uint16_t division);
 	void end();
 	float getHighUsec();
 	float getLowUsec();
-	bool isHighUpdate();
+	bool isUpdate();
 	bool isLowUpdate();
-	inline void __setUpMode();
-	inline void __setDownMode();
 	void __pulseUpAction(uint16_t _upTcnt);
 	void __pulseDownAction(uint16_t _downTcnt);
 
+	static float timeDurationToUsec(timeDuration td);
 	static void __countUp();
 	static bool __isUpMode();
 	static bool __isDownMode();
+	static void __setUpMode();
+	static void __setDownMode();
 };
-uint8_t MeasureRc::pinNum=0;
+uint8_t MeasureRc::measureNum=0;
 uint16_t MeasureRc::nowCount=0;
-MeasureRc *objPtr;
+float MeasureRc::bitToUsec=0;
+MeasureRc *objPtr[32];
 
-MeasureRc::MeasureRc(Pin _pin){	//コンストラクタ(変数の初期化とタイマー設定)
-	objPtr = this;
-	pin = _pin;
-	pinNum++;
+MeasureRc::MeasureRc(avrPin pin){	//コンストラクタ(変数の初期化とタイマー設定)
+	objPtr[measureNum] = this;
+	measureNum++;
+	measurePin = pin;
+	cbi(*(measurePin.ddr), measurePin.pinNum);
+	sbi(*(measurePin.port), measurePin.pinNum);
 	nowCount=0;
-	upTcnt=0;
-	upCount=0;
-	downTcnt=0;
-	downCount=0;
-	highBit=0;
-	highUpdateFlag=false;
-	lowBit=0;
-	lowUpdateFlag=false;
+	updateFlag=false;
 }
 void MeasureRc::begin(uint16_t division=256){
 	Tm1Ctrl::setDivision(division);	//精度12.8us　計測可能時間約55ks=15h
 	division = Tm1Ctrl::getDivision();
-	bitToUsec = division/(F_CPU/1000000.0);	//1bit=12.8us
+	MeasureRc::bitToUsec = division/(F_CPU/1000000.0);	//1bit=12.8us
 	Tm1Ctrl::enableCaptIrpt();
 	Tm1Ctrl::enableOvfIrpt();//ovf割り込み許可
+	MeasureRc::__setUpMode();
 	sei();
 }
 void MeasureRc::end(){
 	Tm1Ctrl::disableCaptIrpt();
 }
 float MeasureRc::getHighUsec(){
-	highUpdateFlag=false;
-	return highBit * bitToUsec;
+	updateFlag=false;
+	return MeasureRc::timeDurationToUsec(bufferOnTime);
 }
-float MeasureRc::getLowUsec(){
-	lowUpdateFlag=false;
-	return lowBit * bitToUsec;
+inline bool MeasureRc::isUpdate(){
+	return updateFlag;
 }
-bool MeasureRc::isHighUpdate(){
-	return highUpdateFlag;
+float MeasureRc::timeDurationToUsec(timeDuration td){
+	if(td.endCount>=td.startCount){
+		return (((float)td.endCount        -(float)td.startCount)*65536.0 + ((float)td.endTcnt-(float)td.startTcnt)) * MeasureRc::bitToUsec;
+	}
+	else{
+		return (((float)td.endCount+65536.0-(float)td.startCount)*65536.0 + ((float)td.endTcnt-(float)td.startTcnt)) * MeasureRc::bitToUsec;
+	}
 }
-bool MeasureRc::isLowUpdate(){
-	return lowUpdateFlag;
+
+inline void MeasureRc::__pulseUpAction(uint16_t upTcnt){
+	onTime.startCount = nowCount;
+	onTime.startTcnt  = upTcnt;
 }
-void MeasureRc::__countUp(){
+inline void MeasureRc::__pulseDownAction(uint16_t downTcnt){
+	onTime.endCount = nowCount;
+	onTime.endTcnt  = downTcnt;
+	bufferOnTime = onTime;	//バッファにコピー
+	updateFlag = true;
+}
+inline void MeasureRc::__countUp(){
 	MeasureRc::nowCount++;
 }
 inline void MeasureRc::__setUpMode(){
@@ -86,62 +113,37 @@ inline void MeasureRc::__setUpMode(){
 inline void MeasureRc::__setDownMode(){
 	cbi(TCCR1B,ICES1);
 }
-bool MeasureRc::__isUpMode(){
+inline bool MeasureRc::__isUpMode(){
 	return checkBit(TCCR1B, ICES1);
 }
-bool MeasureRc::__isDownMode(){
+inline bool MeasureRc::__isDownMode(){
 	return !checkBit(TCCR1B, ICES1);
 }
-void MeasureRc::__pulseUpAction(uint16_t _upTcnt){
-	upCount = nowCount;
-	uint16_t upTcnt  = _upTcnt;
-	if(downTcnt <= upTcnt){
-		lowBit = (uint32_t)(upCount-downCount  )*65536 + upTcnt      -downTcnt;
-	}
-	else{
-		lowBit = (uint32_t)(upCount-downCount-1)*65536 + upTcnt+65536-downTcnt;
-	}
-	lowUpdateFlag = true;
-}
-void MeasureRc::__pulseDownAction(uint16_t _downTcnt){
-	downCount = nowCount;
-	downTcnt  = _downTcnt;
-	if(upTcnt <= downTcnt){
-		highBit = (uint32_t)(downCount-upCount  )*65536 + downTcnt      -upTcnt;
-	}
-	else{
-		highBit = (uint32_t)(downCount-upCount-1)*65536 + downTcnt+65536-upTcnt;
-	}
-	highUpdateFlag = true;
-}
-
-
 
 
 ISR(TIMER1_OVF_vect){
 	MeasureRc::__countUp();
 }
+
 ISR(TIMER1_CAPT_vect){
-	sbi(PORTD,7);
-	uint16_t _icr1 = ICR1;
+	static uint8_t measuringObj=0;
+	sbi(PORTD,4);
+	//uint16_t _icr1 = ICR1;
 
-	for(uint8_t i=0; i<MeasureRc::pinNum; i++){
-		if( checkBit(objPtr->pin.port, objPtr->pin.pinNum)==1 ){
-
-			objPtr->__pulseUpAction(_icr1);
+	if(MeasureRc::__isDownMode()){	//立ち下がったのなら
+		objPtr[measuringObj]->__pulseDownAction(ICR1);
+		MeasureRc::__setUpMode();
+	}
+	else{	//立ちあがったのなら
+		for(uint8_t i=0; i<MeasureRc::measureNum; i++){
+			if( checkBit( *(objPtr[i]->measurePin.pin), objPtr[i]->measurePin.pinNum )==1 ){
+				measuringObj = i;
+				objPtr[measuringObj]->__pulseUpAction(ICR1);
+				MeasureRc::__setDownMode();
+				break;
+			}
 		}
 	}
-/*
-	if(MeasureRc::__isUpMode()){
-
-		MeasurePwmCapt.__pulseUpAction(hoge);
-		MeasurePwmCapt.__setDownMode();
-	}
-	else{
-		MeasurePwmCapt.__pulseDownAction(hoge);
-		MeasurePwmCapt.__setUpMode();
-	}
-*/
-	cbi(PORTD,7);
+	cbi(PORTD,4);
 }
 #endif
